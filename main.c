793df74831646d637867de1a3ca5f3aaa71992b3
Leo <qye50@stuy.edu>
2024-11-25 16:25:24 -0500

    finished child process exiting

--- main.c
+++ main.c
@@ -4,40 +4,46 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 
 int main(){
-  // for (int i = 0; i < 2; i++){
-  //   if (p == o)
-  // }
+  printf("%d about to create 2 child processes\n", getpid());
   pid_t p;
-  p = fork();
-  if(p < 0){
-  perror("fork fail");
-  printf("%s\n", strerror(errno));
-  exit(1);
-  }
-  else if (p != 0){
-    pid_t p;
+  // for (int i = 0; i < 2; i++){
     p = fork();
-  }
-  if (p == 0){
-    // random
-    int fd = open("/dev/random", O_RDONLY);
-    if (!fd){
-      printf("%s\n", strerror(errno));
-      exit(-1);
+    if (p){
+      p = fork();
     }
-    int n  = -1;
-    int r = read(fd, &n, 4);
-    if (!r){
-      printf("%s\n", strerror(errno));
-      exit(-1);
+    if (!p){
+      int fd = open("/dev/random", O_RDONLY);
+      int n  = -1;
+      int r = read(fd, &n, 4);
+      n = abs(n % 5) + 1;
+      // printf("%d\n", n);
+      // printf("%d\n", p);
+      printf("%d %d sec\n", getpid(), n);
+      sleep(n);
+      printf("%d finished after %d seconds.\n", getpid(), n);
+      exit(0);
     }
-    n = abs(n % 5) + 1;
-    printf("%d\n", n);
+  // }
 
-  }
+  // if(p < 0){
+  // perror("fork fail");
+  // printf("%s\n", strerror(errno));
+  // exit(1);
+  // }
+  // else if (p != 0){
+  //   pid_t p;
+  //   p = fork();
+  // }
+  // if (p == 0){
+  //   // random
 
+// }
+int n = 0;
+wait(&n);
+exit(0);
 
 }
